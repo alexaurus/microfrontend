66cdd35373dc2d13d1d02b1388aef93c084e4e85
alexaurus <a.cole.sw@gmail.com>
2022-02-16 17:26:27 -0600

    finished step 1

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <MarketingApp />
+      </div>
+    </div>
+  );
+};
