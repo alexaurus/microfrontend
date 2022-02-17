c55d317d97dffed75d600bdc35e7bd9a656067b6
alexaurus <a.cole.sw@gmail.com>
2022-02-17 17:53:38 -0600

    added haeder

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
+    <BrowserRouter>
       <div>
-        <div>Hello there!</div>
+        <Header />
         <MarketingApp />
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
